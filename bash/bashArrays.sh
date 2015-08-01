d91efe136617dbb8e7ad55714e97228344e3f0d5
George Jones <gmj@pobox.com>
2015-08-01 05:31:03 -0400

    Fixed handling of undefined variables/missing name

--- bash/bashArrays.sh
+++ bash/bashArrays.sh
@@ -5,7 +5,7 @@ arrayCreate() {
     # Ouput: NAME set as global
     # Return: 0 if defined, 1 otherwise.
     #
-    [ -z "${1:-''}" ] && echo "createArray: Missing variable name." 1>&2 && return 1
+    [ -z "${1:-}" ] && echo "createArray: Missing array name." 1>&2 && return 1
     local var=${1}
     shift
     
@@ -27,10 +27,36 @@ isArray() {
     #
     # https://gist.github.com/coderofsalvation/8377369
     #
-    [ -z "${1:-''}" ] && echo "isArray: Missing variable name." 1>&2 && return 1
+    [ -z "${1:-}" ] && echo "isArray: Missing array name." 1>&2 && return 1
     if [ -n "$BASH" ]; then
         # this is officially a hack, but it seems to be the best solution.  Yuck.
         declare -p ${1:-''} 2> /dev/null | grep 'declare \-a' >/dev/null && return 0
     fi
     return 1
+}
+
+
+arrayAppend() {
+    # Append to an array
+    #
+    # Usgae: arrayAppend NAME [element1 element2...]
+    # Ouput: NAME set as global
+    # Return: 0 if defined, 1 otherwise.
+    #
+    [ -z "${1:-}" ] && echo "createAppend: Missing array name." 1>&2 && return 1
+    local var="${1:-''}"
+    shift
+
+    echo var ${var-''}
+    echo varAT ${var[@]}
+    echo args "$@"
+    echo
+    
+    if [ -z "${1:-''}" ]; then
+	true
+    else
+	eval "$var=(\"${var[@]}\"  \"$@\" )"
+    fi
+    
+    return 0
 }
