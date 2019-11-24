80ae383dbc7c4e493074dceca29552c34ed41c6c
George Jones <gmj@pobox.com>
2019-11-24 07:01:12 -0500

    Added history-grep-tail

---
+++ bash/bashFunctionsWithArguments.sh
@@ -0,0 +1,16 @@
+# Define a bash function that takes an argument
+# (in this case, because aliases don't)
+
+
+hgt() {
+    # hgt == "history grep (for arg) tail"
+    #echo "Histroy Grep tail"
+
+    if [ -z ${1+x} ]; then
+        echo 'hgt needs an argument' 1>&2
+        return 1
+    fi
+
+    history | grep -i "$1" | tail
+    return 0
+}
